19646d09f3c567d2362fec3e04950215344982dd
Mattias Carlsson <mattiascson@gmail.com>
2022-10-22 07:08:24 +0200

    Work on PCA

--- script_R/10_PCA.R
+++ script_R/10_PCA.R
@@ -17,18 +17,21 @@ MediatorGenes <- read_csv(file=paste("./InOutFiles/", Date,"_MediatorGenesDebatc
 
 Annotation <- read_csv(file=paste("./InOutFiles/20221001_Annotation.csv",sep=""))
 
+#Read batch file. This file was not 100% correct manually corrected it.
+PrepClust <- read_csv("../InFiles/SF-2245_clusters (003).csv") %>% select(Sample,Cluster)
+#Set no of samples
+NOSamples <- dim(AllIntronGenes)[2]-3
+#Assign samples in rRNA batch. 1 low rRNA 197. 2 high rRNA 283
+PrepClust %<>% slice(match(names(GeneTibble[,3:(NOSamples+2)]),PrepClust[["Sample"]]))
+
+#Restructure/transpose data
+AllIntronGenes %>% select(where(is.numeric) & !LengthDiff)
+
 #Set saved image	(8.27x11.69
 #png(file=paste("./Outpng/", Date,"_med9-vs-Wt_2h4h6h-0h.png",sep=""), width=11.69, height=7.27, units="in", res=100)
 #Set to 3x3 combiplot
 #par(mfrow=c(3,3))
 
-
-
 #Do the plotting
 #dev.off()
 
-Test <- matrix(c(c(1,2,3),
-                 c(4,6,7),
-                 c(8,3,2)),
-byrow = TRUE,ncol=3)
-row.names(Test)
